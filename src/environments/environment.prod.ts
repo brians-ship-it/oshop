3e880d2fe6e2b1e5b1ad6883947fd658a92f07ac
Brian Sithole <briansworkman@gmail.com>
2018-07-05 01:40:05 +0300

    initial commit and firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDOj1MQwu0ooxXYq0_kBGRUbvhlKaUJ8n0",
+    authDomain: "oshop-c3313.firebaseapp.com",
+    databaseURL: "https://oshop-c3313.firebaseio.com",
+    projectId: "oshop-c3313",
+    storageBucket: "oshop-c3313.appspot.com",
+    messagingSenderId: "772222768445"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDOj1MQwu0ooxXYq0_kBGRUbvhlKaUJ8n0",
+    authDomain: "oshop-c3313.firebaseapp.com",
+    databaseURL: "https://oshop-c3313.firebaseio.com",
+    projectId: "oshop-c3313",
+    storageBucket: "oshop-c3313.appspot.com",
+    messagingSenderId: "772222768445"
+  }
 };
 
 /*
